a6717905d3ec07a38add11d25c1c42bbd5d9357a
ajz34 <ajz34@outlook.com>
2024-12-31 19:03:28 +0800

    update welcome message

---
+++ listings/features-default/tests/welcome.rs
@@ -0,0 +1,32 @@
+use rstsr_core::prelude::*;
+
+#[test]
+fn welcome() {
+    // 3x2 matrix with c-contiguous memory layout
+    let a = rt::asarray((vec![6., 2., 7., 4., 8., 5.], [3, 2].c()));
+
+    // 2x4x3 matrix by arange and reshaping
+    let b = rt::arange(24.).into_shape([2, 4, 3]);
+
+    // broadcasted matrix multiplication
+    let c = b % a;
+
+    // print layout of the result
+    println!("{:?}", c.layout());
+    // output:
+    // 3-Dim, contiguous: Cc
+    // shape: [2, 4, 2], stride: [8, 2, 1], offset: 0
+
+    // print the result
+    println!("{:6.1}", c)
+    // output:
+    // [[[   23.0   14.0]
+    //   [   86.0   47.0]
+    //   [  149.0   80.0]
+    //   [  212.0  113.0]]
+    //
+    //  [[  275.0  146.0]
+    //   [  338.0  179.0]
+    //   [  401.0  212.0]
+    //   [  464.0  245.0]]]
+}
--- src/welcome.md
+++ src/welcome.md
@@ -1,1 +1,9 @@
-Welcome to RSTSR, a tensor library for rust.
+# Welcome to RSTSR
+
+Welcome to RSTSR, a rust tensor toolkit library.
+
+To start with, you may try to run the following code:
+
+```rust
+{{#include ../listings/features-default/tests/welcome.rs}}
+```
