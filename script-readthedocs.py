1b7765662d93d569ab90281ea735e8b712946f02
ajz34 <ajz34@outlook.com>
2025-02-10 13:06:56 +0800

    try versioning

--- script-readthedocs.py
+++ script-readthedocs.py
@@ -2,7 +2,7 @@ with open("docusaurus.config.js", "r") as f:
     token = f.read()
 
 token = token.replace("url: 'https://ajz34.github.io'", "url: 'https://rstsr-book.readthedocs.io'")
-token = token.replace("baseUrl: '/rstsr-book/'", "baseUrl: '/latest/'")
+token = token.replace("baseUrl: '/rstsr-book/'", "baseUrl: '/v0.0.1/'")
 
 with open("docusaurus.config.js", "w") as f:
     f.write(token)
